0a66620d75002b8d3dfe3d8f397e77dcdeffabfa
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-09-22 00:32:33 +0700

    Test tailwind

--- nebula/src/pages/_app.js
+++ nebula/src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+        rel="stylesheet"
+      /> */}
+      <Component {...pageProps} />
+    </>
+  );
 }
