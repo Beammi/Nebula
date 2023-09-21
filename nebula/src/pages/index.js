fe2fbd4a261ef1b4f187bf36627a9b630f77e8b6
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-09-22 00:53:01 +0700

    Tes DaisyUI

--- nebula/src/pages/index.js
+++ nebula/src/pages/index.js
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
   return (
@@ -9,7 +9,11 @@ export default function Home() {
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="font-sans">Hello World</div>
+        <div className="btn bg-blue text-white font-sans">
+          <span className="uppercase">MIXED</span>
+          <span className="lowercase">Case</span>
+          <span className="uppercase">Button</span>
+        </div>
       </div>
     </main>
   )
