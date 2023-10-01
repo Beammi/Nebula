0f4eaea8d6f43bebae2314ed16678b920e4d3937
natthapong2100 <natthapong2100@gmail.com>
2023-10-02 00:39:15 +0700

    register test prototype

---
+++ nebula/src/pages/register.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Register() {
+  return (
+    <>
+        {/* <div className='flex items-center justify-center min-h-screen bg-blue'> */}
+        <div className='flex justify-center min-h-screen bg-white'>
+            <h2 className='mb-5 font-bold text-4xl text-blue'>Register</h2>   
+        </div>
+    </>
+  )
+}
