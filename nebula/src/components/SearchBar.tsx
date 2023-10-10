e8c0ba86d7773c6b3dfc9b00c865ee782516a924
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-10-10 19:40:54 +0700

    Fix responsive position of SearchBar

--- nebula/src/components/SearchBar.tsx
+++ nebula/src/components/SearchBar.tsx
@@ -5,7 +5,7 @@ interface ISearchBar {
 const SearchBar: React.FunctionComponent<ISearchBar> = ({ text }) => {
   return (
     
-      <div className="relative w-full p-4 max-w-md sm:w-3/4 lg:w-1/3 xl:w-1/3 ">
+      <div className="relative w-full p-4 max-w-md sm:w-3/4">
         <input
           type="text"
           className="pl-10 pr-4 py-2 w-full input input-bordered "
