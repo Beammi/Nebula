db3500eb6bc6bc87233d11b185740142c8f00221
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-10-10 19:41:40 +0700

    Test Map

---
+++ nebula/src/components/MyMap.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyMap from './MyMap';
+
+const MyApp: React.FC = () => {
+  return (
+    <div>
+      <h1>My Map App</h1>
+      <MyMap />
+    </div>
+  );
+};
+
+export default MyApp;
