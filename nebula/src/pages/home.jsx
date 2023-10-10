070528c51bc507dcc914e9c80d8d562d792b19d6
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-10-10 19:41:53 +0700

    Add Map to Home Page

--- nebula/src/pages/home.jsx
+++ nebula/src/pages/home.jsx
@@ -1,10 +1,17 @@
-import SearchBar from "@/components/SearchBar"
-import ProfileButton from "@/components/ProfileButton"
+import SearchBar from "@/components/SearchBar";
+import ProfileButton from "@/components/ProfileButton";
+import DynamicMap from "@/components/DynamicMap";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-between p-4">
-      <SearchBar text="Search"></SearchBar>
-      <ProfileButton text="NL"></ProfileButton>
+    <div className="relative h-screen">
+      <div className="absolute z-10 top-0 left-0 right-0 flex items-center justify-between p-4">
+        <SearchBar text="Search" />
+        <ProfileButton text="NL" />
+      </div>
+      <div className="absolute z-0 w-full h-full">
+        <DynamicMap />
+      </div>
     </div>
-  )
+  );
 }
