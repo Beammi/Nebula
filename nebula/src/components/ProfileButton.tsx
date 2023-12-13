f0ec71931010ee9246e9a5ff0b35a2f52c89d190
Somboon Siriwit <goawayez@gmail.com>
2023-12-13 16:11:20 +0700

    Add dropdown to profile and reorder component

--- nebula/src/components/ProfileButton.tsx
+++ nebula/src/components/ProfileButton.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface IProfileButton {
     text?: string
   }
@@ -5,11 +7,20 @@ interface IProfileButton {
   const ProfileButton: React.FunctionComponent<IProfileButton> = ({ text }) => {
     const colors = ['bg-blue','bg-red','bg-yellow','bg-dark-grey']
     const randomColor = colors[Math.floor(Math.random() * colors.length)]
+    const [IsOpen, setIsOpen] = useState(false)
     return (
         <div className="p-4">
-          <button className={`btn btn-circle text-white ${randomColor} p-4`}>
+          <button className={`btn btn-circle text-white ${randomColor} p-4`} onClick={() => setIsOpen(!IsOpen)}>
             {text}
           </button>
+          <div className={`flex flex-col bg-white absolute right-12 p-8 shadow-lg rounded-lg opacity-0 top-24 transition-all ease-in duration-200 ${IsOpen ? 'opacity-100' : 'right-[-200px]'}`}>
+              <ul className="flex flex-col gap-4 text-[black]">
+                  <li>Profile</li>
+                  <li>Your Nebu</li>
+                  <li>Your Tour</li>
+                  <li>Setting</li>
+              </ul>
+          </div>
         </div>
       
     )
--- nebula/src/pages/home.jsx
+++ nebula/src/pages/home.jsx
@@ -1,13 +1,29 @@
 import SearchBar from "@/components/SearchBar";
 import ProfileButton from "@/components/ProfileButton";
 import DynamicMap from "@/components/DynamicMap";
+import Button from "@/components/Button"
 
 export default function Home() {
+
   return (
     <div className="relative h-screen">
       <div className="absolute z-10 top-0 left-0 right-0 flex items-center justify-between p-4">
+        <div className="flex">
+          <SearchBar text="Search" />
+          <Button buttonStyle="btn btn-primary bg-yellow w-fit md:block hidden mx-2" label="Cafe with wifi"></Button>
+          <Button buttonStyle="btn btn-primary bg-yellow w-fit md:block hidden" label="Restaurant"></Button>
+        </div>
+        <div className="flex">
+          <div className="flex">
+            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden mx-2" label="Create Tour"></Button>
+            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden" label="Add Nebu"></Button>
+          </div>
+          <ProfileButton text="NL"/>
+        </div>
+        {/*
         <SearchBar text="Search" />
-        <ProfileButton text="NL" />
+        <ProfileButton text="NL"/>
+        */}
       </div>
       <div className="absolute z-0 w-full h-full">
         <DynamicMap />
