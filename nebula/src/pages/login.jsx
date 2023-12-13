f9fabc4331d4b1cc617bb0c45e4c6d8c1508da1b
Natthapong Lueangphumphitthaya <natthapong2100@gmail.com>
2023-12-14 02:30:35 +0700

    change page height and add apps platform authen

--- nebula/src/pages/login.jsx
+++ nebula/src/pages/login.jsx
@@ -5,45 +5,74 @@ import timeAdventurePic from '../../public/images/time_adventure.png'
 import Footer from '../components/Footer'
 import Button from '../components/Button'
 import Navbar from '@/components/Navbar'
-import TextInput from '@/components/TextInput'
+import TextInput from '@/components/TextInputWithLabel'
+import googleIcon from '../../public/images/google-icon.png'
+import facebookIcon from '../../public/images/fb-icon.png'
+import twitterIcon from '../../public/images/twitter-icon.png'
 
 export default function Login() {
   return (
     <>
-        <Navbar />
-        <div className="hero p-10 bg-white">
-          <div className="hero-content flex-col md:flex-row gap-32">
+        <div className='h-screen'>
+          <Navbar />
+          <div className="hero p-10 bg-white h-fit min-h-screen">
+            <div className="hero-content flex-col md:flex-row gap-32">
 
-            <figure><Image src={timeAdventurePic} alt="adventure pic" className="pt-2" width={520}/></figure>
+              <figure><Image src={timeAdventurePic} alt="adventure pic" className="pt-2" width={520}/></figure>
 
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-white border-2 border-black text-black">
-              <form className="card-body">
+              <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-white border-2 border-black text-black">
+                <form className="card-body">
 
-                <h2 className='font-bold text-2xl text-center'>Login Natthapong</h2>
-                <div className="divider before:bg-grey after:bg-grey"></div>
-                                 
-                <TextInput textLabel="Email Address" inputType="email" placeholder="Enter email"/>                
-                <TextInput textLabel="Password" inputType="password" placeholder="Enter Password"/>
+                  <h2 className='font-bold text-2xl text-center'>Login</h2>
+                  <div className="divider before:bg-grey after:bg-grey"></div>
+                                  
+                  <TextInput textLabel="Email Address" inputType="email" placeholder="Enter email"/>                
+                  <TextInput textLabel="Password" inputType="password" placeholder="Enter Password"/>
 
-                <div className="flex justify-center">
-                  <Button buttonStyle="btn btn-primary bg-blue w-fit" label="Login"></Button>
-                </div>
+                  <div className="flex justify-center">
+                    <Button buttonStyle="btn btn-primary bg-blue w-fit" label="Login"></Button>
+                  </div>
 
-                <div className='flex justify-center'>
-                  <label className="label">
-                    <label className='font-normal text-sm'>Don't have an account?</label>
-                    <a href="/register" className="label-text-alt link link-hover indent-1 text-blue text-sm">Register</a>
-                  </label>
-                </div>
+                  <div className='flex justify-center'>
+                    <label className="label">
+                      <label className='font-normal text-sm'>Don't have an account?</label>
+                      <a href="/register" className="label-text-alt link link-hover indent-1 text-blue text-sm">Register</a>
+                    </label>
+                  </div>
 
-              </form>
+                  <div className="divider before:bg-grey after:bg-grey"></div>
+                  <div className='flex flex-col justify-center text-center'>
+                    {/* <label className="label"> */}
+                      <label className='font-normal text-lg mb-2'>Or Login with</label>
+                      <div className='flex flex-col justify-center md:flex-row md:gap-x-2'>
+                        <div className='flex flex-row items-center border-2 rounded-md px-2'>
+                          <figure><Image src={googleIcon} alt="adventure pic" className="py-2 mr-1" width={20}/></figure>
+                          <label>Google</label>
+                        </div>
+
+                        <div className='flex flex-row items-center border-2 rounded-md px-2'>
+                          <figure><Image src={facebookIcon} alt="adventure pic" className="py-2 mr-1" width={20}/></figure>
+                          <label>Facebook</label>
+                        </div>
+
+                        <div className='flex flex-row items-center border-2 rounded-md px-2'>
+                          <figure><Image src={twitterIcon} alt="adventure pic" className="py-2 mr-1" width={20}/></figure>
+                          <label>Twitter</label>
+                        </div>
+                      </div>
+                    {/* </label> */}
+                  </div>
+
+
+                </form>
+              </div>
+
             </div>
-
           </div>
+
+
+          <Footer />
         </div>
-
-
-        <Footer />
     </>
   )
 }
--- nebula/src/pages/register.jsx
+++ nebula/src/pages/register.jsx
@@ -10,42 +10,44 @@ import TextInput from '@/components/TextInputWithLabel'
 export default function Register() {
   return (
     <>
-        <Navbar />
-        <div className="hero p-10 bg-white">
-          <div className="hero-content flex-col md:flex-row gap-32">
+        <div className='h-screen'>
+          <Navbar />
+          <div className="hero p-10 bg-white h-fit min-h-screen">
+            <div className="hero-content flex-col md:flex-row gap-32">
 
-            <figure><Image src={readyAdventurePic} alt="adventure pic" className="pt-2" width={520}/></figure>
+              <figure><Image src={readyAdventurePic} alt="adventure pic" className="pt-2" width={520}/></figure>
 
-            <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-white border-2 border-black text-black">
-              <form className="card-body">
+              <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-white border-2 border-black text-black">
+                <form className="card-body">
 
-                <h2 className='font-bold text-2xl text-center'>Register</h2>
-                <div className="divider before:bg-grey after:bg-grey"></div>                 
+                  <h2 className='font-bold text-2xl text-center'>Register</h2>
+                  <div className="divider before:bg-grey after:bg-grey"></div>                 
 
-                <TextInput textLabel="Email Address" inputType="email" placeholder="Enter email"/>                
-                <TextInput textLabel="Password" inputType="password" placeholder="Enter Password"/>
-                <TextInput textLabel="Confirm Password" inputType="password" placeholder="Confirm Password"/>
+                  <TextInput textLabel="Email Address" inputType="email" placeholder="Enter email"/>                
+                  <TextInput textLabel="Password" inputType="password" placeholder="Enter Password"/>
+                  <TextInput textLabel="Confirm Password" inputType="password" placeholder="Confirm Password"/>
 
-                <div className="flex justify-center">
-                  {/* <button className="btn btn-primary bg-blue w-fit">Register</button> */}
-                  <Button buttonStyle="btn btn-primary bg-blue w-fit" label="Register"></Button>
-                </div>
+                  <div className="flex justify-center">
+                    {/* <button className="btn btn-primary bg-blue w-fit">Register</button> */}
+                    <Button buttonStyle="btn btn-primary bg-blue w-fit" label="Register"></Button>
+                  </div>
 
-                <div className='flex justify-center'>
-                  <label className="label">
-                    <label className='font-normal text-sm'>Have an account?</label>
-                    <a href="/login" className="label-text-alt link link-hover indent-1 text-blue text-sm">Log In</a>
-                  </label>
-                </div>
+                  <div className='flex justify-center'>
+                    <label className="label">
+                      <label className='font-normal text-sm'>Have an account?</label>
+                      <a href="/login" className="label-text-alt link link-hover indent-1 text-blue text-sm">Log In</a>
+                    </label>
+                  </div>
 
-              </form>
+                </form>
+              </div>
+
             </div>
-
           </div>
+
+
+          <Footer />
         </div>
-
-
-        <Footer />
     </>
   )
 }
