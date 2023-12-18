9e05825b8cae1e1226c314df464836a2086e0d47
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2023-12-18 22:41:29 +0700

    Test GET api

--- nebula/src/pages/api/users/index.js
+++ nebula/src/pages/api/users/index.js
@@ -1,29 +1,12 @@
-import prisma from '../../../lib/prisma.js';
+// pages/api/users.js
+const db = require('../../../lib/db');
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { name, email } = req.body;
-    console.log("Request Body:", req.body); // Add this line
-
-
-    // Validate the input
-    if (!name || !email) {
-      return res.status(400).json({ error: 'Missing name or email' });
-    }
-
-    try {
-      const newUser = await prisma.user.create({
-        data: {
-          name,
-          email,
-        },
-      });
-      return res.status(201).json(newUser);
-    } catch (error) {
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-  } else {
-    // Handle other HTTP methods or return an error
-    res.status(405).end();
+  try {
+    const { rows } = await db.query('SELECT * FROM users');
+    res.status(200).json(rows);
+  } catch (err) {
+    console.error('Database query error', err);
+    res.status(500).json({ error: 'Internal server error' });
   }
 }
