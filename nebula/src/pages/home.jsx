6e575bd7f38357ca29ced5cb91be6052ed07b554
Natthapong Lueangphumphitthaya <natthapong2100@gmail.com>
2023-12-18 23:53:29 +0700

    add normal case of button

--- nebula/src/pages/home.jsx
+++ nebula/src/pages/home.jsx
@@ -18,13 +18,13 @@ export default function Home() {
       <div className="absolute z-10 top-0 left-0 right-0 flex items-center justify-between p-4">
         <div className="flex">
           <SearchBar text="Search" />
-          <Button buttonStyle="btn btn-primary bg-yellow w-fit md:block hidden mx-2 border-none" label="Cafe with wifi"></Button>
-          <Button buttonStyle="btn btn-primary bg-yellow w-fit md:block hidden border-none" label="Restaurant"></Button>
+          <Button buttonStyle="btn bg-yellow hover:bg-dark-grey w-fit md:block hidden mx-2 border-none text-black normal-case" label="Café with wifi"></Button>
+          <Button buttonStyle="btn bg-yellow hover:bg-dark-grey w-fit md:block hidden border-none text-black normal-case" label="Restaurant"></Button>
         </div>
         <div className="flex">
           <div className="flex">
-            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden mx-2" label="Create Tour"></Button>
-            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden" label="Add Nebu" onClick={openAddNebu}></Button>
+            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden mx-2 normal-case" label="Create Tour"></Button>
+            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden normal-case" label="Add Nebu" onClick={openAddNebu}></Button>
           </div>
           <ProfileButton text="NL"/>
         </div>
