090fb70dcdbbce40d4bb065238ea33593d1da6fe
Natthapong Lueangphumphitthaya <natthapong2100@gmail.com>
2023-12-20 00:17:50 +0700

    shift a little of filter button

--- nebula/src/pages/home.jsx
+++ nebula/src/pages/home.jsx
@@ -18,8 +18,8 @@ export default function Home() {
       <div className="absolute z-10 top-0 left-0 right-0 flex items-center justify-between p-4">
         <div className="flex">
           <SearchBar text="Search" />
-          <Button buttonStyle="btn bg-yellow hover:bg-dark-grey w-fit md:block hidden mx-2 border-none text-black normal-case" label="Café with wifi"></Button>
-          <Button buttonStyle="btn bg-yellow hover:bg-dark-grey w-fit md:block hidden border-none text-black normal-case" label="Restaurant"></Button>
+          <Button buttonStyle="btn bg-yellow hover:bg-dark-grey w-fit md:block hidden mx-8 border-none text-black normal-case" label="Café with wifi"></Button>
+            <Button buttonStyle="btn bg-yellow hover:bg-dark-grey w-fit md:block hidden border-none text-black normal-case" label="Restaurant"></Button>
         </div>
         <div className="flex">
           <div className="flex">
