ab00eeb2cb7a28abfaada4779d5099a7ec533bea
Somboon-Siriwit <91792909+Somboon-Siriwit@users.noreply.github.com>
2023-12-21 01:15:09 +0700

    Profile button in search bar

--- nebula/src/pages/home.jsx
+++ nebula/src/pages/home.jsx
@@ -15,28 +15,28 @@ export default function Home() {
 
   return (
     <div className="relative h-screen">
-      <div className="absolute z-10 top-0 left-0 right-0 flex items-center justify-between p-4">
-        <div className="flex">
+      <div className="absolute z-10 top-0 left-0 right-0 flex items-center justify-center md:justify-between p-4">
+        <div className="flex w-full justify-center md:justify-normal">
           <SearchBar text="Search" />
           <Button buttonStyle="btn btn-primary bg-yellow w-fit md:block hidden mx-2 border-none" label="Cafewithwifi"></Button>
           <Button buttonStyle="btn btn-primary bg-yellow w-fit md:block hidden border-none" label="Restaurant"></Button>
         </div>
         <div className="flex">
           <div className="flex">
-            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden mx-2" label="Create Tour"></Button>
-            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden" label="Add Nebu" onClick={openAddNebu}></Button>
+            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden mx-2" label="CreateTour"></Button>
+            <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden" label="AddNebu" onClick={openAddNebu}></Button>
           </div>
           <ProfileButton text="NL"/>
         </div>
-        {/*
-        <SearchBar text="Search" />
-        <ProfileButton text="NL"/>
-        */}
       </div>
       <div className="absolute z-0 w-full h-full">
         <DynamicMap />
       </div>
       <AddNebu toggle={addNebuState} action={openAddNebu} />
+      <div className="absolute right-24 top-24 text-center text-white bg-blue flex flex-col rounded-lg font-bold items-center overflow-hidden hidden">
+      <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden border-none" label="Current Location"></Button>
+      <Button buttonStyle="btn btn-primary bg-blue w-fit md:block hidden mb-4 border-none px-5" label="Search Location"></Button>
+      </div>   
     </div>
   );
 }
