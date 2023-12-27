fa257c67e85e1c16d5e44d294f556e306f8616e4
Natthapong Lueangphumphitthaya <natthapong2100@gmail.com>
2023-12-27 14:51:38 +0700

    place info panel in others section prototype ver.1

--- nebula/src/components/PlaceInfoPanel.tsx
+++ nebula/src/components/PlaceInfoPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image, { StaticImageData } from 'next/image'
 
 import towerBridgePic from "../../public/images/tower-bridge-pic.png"
@@ -15,34 +15,59 @@ import smallPhone from "../../public/images/small-phone.png"
 import Link from 'next/link'
 
 import Button from "./Button"
+import { StaticImport } from 'next/dist/shared/lib/get-img-props'
 
-interface PlaceInfoPanelProps { 
-  placeData: { // not necessary to know lat, lon
-    name: string;
-    description: string;
-    image?: StaticImageData;
-    pinSize?: string;
-    // Add other properties as needed
-  } | null;
-  toggle: boolean;
-  action?: any;
-}
+// interface PlaceInfoPanelProps { 
+//   placeData: { // not necessary to know lat, lon
+//     name: string;
+//     description: string;
+//     image?: StaticImageData;
+//     pinSize?: string;
+//     // Add other properties as needed
+//   } | null;
+//   toggle: boolean;
+//   action?: any;
+// }
   
-const PlaceInfoPanel: React.FC<PlaceInfoPanelProps> = ({toggle, action, placeData }) => {
+export default function PlaceInfoPanel({toggle, action, placeData}) {
+
+  const [overviewSection, setOverviewSection] = useState(false)
+  const [rateCommentSection, setRateCommentSection] = useState(false)
+  const [othersNebuSection, setOthersNebuSection] = useState(false)
+
+  function openOverviewSection(){
+    setOverviewSection(true)
+    setRateCommentSection(false)
+    setOthersNebuSection(false)
+  }
+
+  function openRateCommentSection(){
+    setOverviewSection(false)
+    setRateCommentSection(true)
+    setOthersNebuSection(false)
+  }
+
+  function opneOthersNebuSection(){
+    setOverviewSection(false)
+    setRateCommentSection(false)
+    setOthersNebuSection(true)
+  }
+  
 
   return (
-    <div className={`absolute overflow-y-scroll z-10 w-[407px] h-screen rounded-sm bg-white text-black transition-all duration-300 ease-in-out 
+    <div className={`absolute overflow-y-scroll 2xl:w-1/4 lg:w-1/3 z-10 h-screen rounded-sm bg-white text-black transition-all duration-300 ease-in-out 
       ${toggle ? "opacity-100 drop-shadow-2xl" : "hidden"}`}>
       
       <div className=' text-black '>
       
         {placeData ? (
           <div>
-            <figure><Image src={towerBridgePic} alt="pic" className="pt-0 mb-1" width={407}/></figure>
+            
+            <figure><Image src={towerBridgePic} alt="pic" className="pt-0 mb-1 w-full"/></figure>
             <div className='flex flex-col pl-4 pt-2 gap-y-1'>
               <div className='flex flex-row'>
                 <h3 className='font-bold text-xl text-black  bg-white w-fit'>{placeData.name}</h3>
-                <figure className='ml-auto'><Image src={shareIcon} alt="pic" className="mr-4" width={31}/></figure>                
+                {/* <figure className='ml-auto'><Image src={shareIcon} alt="pic" className="mr-4" width={31}/></figure> */}
               </div>
 
               <div className='flex flex-row'>
@@ -58,10 +83,7 @@ const PlaceInfoPanel: React.FC<PlaceInfoPanelProps> = ({toggle, action, placeDat
               </div>
 
               <div className='flex flex-row mt-1 gap-x-1 overflow-x-auto'>
-                <button className="btn btn-outline btn-sm text-red rounded-2xl normal-case hover:bg-light-grey ">
-                  <figure ><Image src={directionsIcon} alt="pic" className="" width={23}/></figure>
-                  Directions
-                </button>
+                
 
                 <button className="btn btn-outline btn-sm text-blue rounded-2xl normal-case hover:bg-light-grey">
                   <figure ><Image src={recommendIcon} alt="pic" className="" width={14} height={21}/></figure>
@@ -71,6 +93,11 @@ const PlaceInfoPanel: React.FC<PlaceInfoPanelProps> = ({toggle, action, placeDat
                 <button className="btn btn-outline btn-sm text-black rounded-2xl normal-case hover:bg-light-grey">
                   <figure ><Image src={bookmarkIcon} alt="pic" className="" width={26} height={26}/></figure>
                   Save
+                </button>
+
+                <button className="btn btn-outline btn-sm text-black rounded-2xl normal-case hover:bg-light-grey ">
+                  <figure ><Image src={shareIcon} alt="pic" className="" width={23}/></figure>
+                  Share
                 </button>
               </div>
 
@@ -90,13 +117,17 @@ const PlaceInfoPanel: React.FC<PlaceInfoPanelProps> = ({toggle, action, placeDat
             <div className='flex flex-col pt-2'>
                 <div className='flex flex-row mt-4 ml-7 gap-x-7'>
                   <div className='flex flex-col items-center justify-end'>
-                    <p className='text-center w-min leading-4 text-blue'>Overview</p>
-                    <div className='w-14 h-1.5 bg-blue rounded-md mt-4'></div>
+                    {/* <p className='text-center w-min leading-4 text-blue'>Overview</p>
+                    <div className='w-14 h-1.5 bg-blue rounded-md mt-4'></div> */}
+                    <button className='btn normal-case bg-white text-blue hover:bg-white hover:text-blue border-x-0 border-t-0 border-b-4 font-medium rounded-none'
+                      onClick={openOverviewSection}>Overview</button>
                   </div>
 
                   <div className='flex flex-col items-center justify-end'>
-                    <p className='text-center w-min leading-4'>Rate & Comment</p>
-                    <div className='w-14 h-1.5 bg-white rounded-md mt-2'></div>
+                    {/* <p className='text-center w-min leading-4'>Rate & Comment</p>
+                    <div className='w-14 h-1.5 bg-white rounded-md mt-2'></div> */}
+                    <button className='btn normal-case bg-white text-black hover:bg-white hover:text-blue border-x-0 border-t-0 border-b-4 font-medium rounded-none'
+                      onClick={openRateCommentSection}>Rate & Comment</button>
                   </div>
 
                   <div className='flex flex-col items-center justify-end'>
@@ -112,7 +143,8 @@ const PlaceInfoPanel: React.FC<PlaceInfoPanelProps> = ({toggle, action, placeDat
                 <div className="w-full h-[3px] bg-grey mt-10"></div>
             </div>
 
-            <div className='flex flex-col mt-8 ml-7 gap-y-6'>
+            {overviewSection && 
+              <div className='flex flex-col mt-8 ml-7 gap-y-6'>
                 <div className='flex flex-row'>
                   <figure className=''><Image src={smallPin} alt="pic" className="mr-4" width={16} height={22}/></figure>
                   <p className='leading-5'>Tower Bridge Rd, London SE1 2UP, United Kingdom</p>
@@ -137,7 +169,37 @@ const PlaceInfoPanel: React.FC<PlaceInfoPanelProps> = ({toggle, action, placeDat
                   <figure className=''><Image src={smallPhone} alt="pic" className="mr-4" width={18.6} height={18.6}/></figure>
                   <p className='leading-5'>+44 2074033761</p>
                 </div>
-            </div>
+              </div>
+            }
+
+            {rateCommentSection && 
+              <div className='flex flex-col mt-8 ml-7 gap-y-6'>
+                <div className='flex flex-row'>
+                  <figure className=''><Image src={smallPin} alt="pic" className="mr-4" width={16} height={22}/></figure>
+                  <p className='leading-5'>"At Rate and Comment" Tower Bridge Rd, London SE1 2UP, United Kingdom</p>
+                </div>
+
+                <div className='flex flex-row'>
+                  <figure className=''><Image src={smallShop} alt="pic" className="mr-4" width={22} height={20}/></figure>
+                  <p className='leading-5'>Mon - Sat, 8.00 - 22.00</p>
+                </div>
+
+                <div className='flex flex-row'>
+                  <figure className=''><Image src={smallClock} alt="pic" className="mr-4" width={19.7} height={18}/></figure>
+                  <p className='leading-5'>End this Monday</p>
+                </div>
+
+                <div className='flex flex-row'>
+                  <figure className=''><Image src={smallWorld} alt="pic" className="mr-4" width={19} height={19}/></figure>
+                  <p className='leading-5'>http://www.towerbridge.org.uk/</p>
+                </div>
+
+                <div className='flex flex-row'>
+                  <figure className=''><Image src={smallPhone} alt="pic" className="mr-4" width={18.6} height={18.6}/></figure>
+                  <p className='leading-5'>+44 2074033761</p>
+                </div>
+              </div>
+            }
             
             {/* Add other information as needed */}
           </div>
@@ -151,5 +213,3 @@ const PlaceInfoPanel: React.FC<PlaceInfoPanelProps> = ({toggle, action, placeDat
     </div>
   );
 };
-
-export default PlaceInfoPanel;
