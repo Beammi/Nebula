ab7e718768faffd471ea3e939b019f4d65e5f78e
Somboon-Siriwit <91792909+Somboon-Siriwit@users.noreply.github.com>
2023-12-27 17:49:27 +0700

    implement NebuTag to AddNebu

--- nebula/src/components/NebuTag.tsx
+++ nebula/src/components/NebuTag.tsx
@@ -1,23 +1,113 @@
-export default function NebuTag(props) {
-  const OpenTag = props.toggle;
+// NebuTag.jsx
+import React, { useRef, useState } from "react";
+import closeIcon from "../../public/images/close.png";
+import Image from "next/image";
+
+const NebuTag = (props) => {
+  const { toggle, action, onConfirm } = props;
+  const isOpen = toggle || false;
+
+  const suggestions = ["Technology", "Nature", "Food", "Travel"];
+  const [isFocus, setIsFocus] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [officialTags, setOfficialTags] = useState([]);
+  const [additionalTag, setAdditionalTag] = useState("");
+  const [additionalTags, setAdditionalTags] = useState([]);
+  const [isHovered, setIsHovered] = useState(false);
+  const inputRef = useRef(null);
+
+  const handleConfirm = () => {
+    {/* 
+    setOfficialTags((prevTags) => [...prevTags, inputValue]);
+    setAdditionalTags((prevTags) => [...prevTags, additionalTag]);
+    setInputValue(""); // Clear Official's Tag input value
+    setAdditionalTag(""); // Clear Additional Tag input value
+    onConfirm([inputValue], [additionalTag]);
+    */}
+
+    if (inputValue.length === 0){
+      alert("Please input at least 1 Official's Tag")
+      return
+    }
+
+    if (inputValue.trim() !== "") {
+      setOfficialTags((prevTags) => [...prevTags, inputValue]);
+      setInputValue("");
+    }
+
+    if (additionalTag.trim() !== ""){
+      setAdditionalTags((prevTags) => [...prevTags, additionalTag]);
+      setAdditionalTag("");
+    }
+
+    if (inputValue.trim() !== "" || additionalTag.trim() !== "") {
+      props.onConfirm([inputValue], [additionalTag]);
+    }
+  };
 
   return (
-    <div
-      className={`fixed top-1/2 left-1/2 tranforms -translate-x-1/2 -translate-y-1/2 transition-all ease-in duration-500 ${
-        OpenTag ? "visible opacity-100 drop-shadow-2xl" : "invisible opacity-0"
-      } `}
-    >
-      <div className="flex flex-col bg-red p-4">
-        <div className="mb-4">
-          <h3>Official's Tag</h3>
-          <input type="text" />
+    <div className={`fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 transition-all ease-in duration-500 ${isOpen ? "visible opacity-100 drop-shadow-2xl" : "invisible opacity-0"}`}>
+      <div className="flex flex-col p-6 bg-red">
+        <div className="flex justify-end mb-2">
+          <button onClick={(event) => { event.preventDefault(); action(); }}>
+            <Image src={closeIcon} alt="Close button" className="pt-2" width={20} />
+          </button>
         </div>
-        <div className="mb-4">
-          <h3>Additional Tag</h3>
-          <input type="text" />
+        <div className="my-2 relative">
+          <p>Official's Tag</p>
+          <input
+            className="bg-white text-black p-1 focus:outline-none focus:border-blue focus:ring-2 focus:ring-blue rounded-md"
+            type="text"
+            onFocus={() => setIsFocus(true)}
+            onBlur={() => { if (!isHovered) setIsFocus(false); }}
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            ref={(el) => { inputRef.current = el; }}
+          />
+          {isFocus && (
+            <div
+              className="shadow-lg absolute w-full"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            >
+              {suggestions.map((suggestion, index) => {
+                const isMatch = suggestion.toLowerCase().indexOf(inputValue.toLowerCase()) > -1;
+                return (
+                  <div key={index}>
+                    {isMatch && (
+                      <div
+                        className="p-2 bg-white hover:bg-blue cursor-pointer"
+                        onClick={() => {
+                          setInputValue(suggestion);
+                          inputRef.current.focus();
+                        }}
+                      >
+                        {suggestion}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          )}
         </div>
-        <button>Confirm</button>
+        <div className="my-2">
+          <p>Additional Tag</p>
+          <input
+            className="bg-white text-black p-1 focus:outline-none focus:border-blue focus:ring-2 focus:ring-blue rounded-md"
+            type="text"
+            value={additionalTag}
+            onChange={(e) => setAdditionalTag(e.target.value)}
+          />
+        </div>
+        <div className="flex justify-center mt-4">
+          <button className="bg-white p-2 rounded-md" onClick={(event) => { event.preventDefault(); handleConfirm(); }}>
+            Confirm
+          </button>
+        </div>
       </div>
     </div>
   );
-}
+};
+
+export default NebuTag;
--- nebula/src/components/SearchBar.tsx
+++ nebula/src/components/SearchBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Button from "./Button"
 
 interface ISearchBar {
@@ -5,6 +6,9 @@ interface ISearchBar {
 }
 
 const SearchBar: React.FunctionComponent<ISearchBar> = ({ text }) => {
+
+  const [IsOpen, setIsOpen] = useState(false)
+
   return (
     
       <div className="relative w-full p-4 max-w-md sm:w-3/4 ">
@@ -19,8 +23,16 @@ const SearchBar: React.FunctionComponent<ISearchBar> = ({ text }) => {
           className="absolute left-6 top-1/2 transform -translate-y-1/2 w-6 h-6"
         />
         <div className="absolute right-8 top-8 transform -translate-y-1/2">
-          <Button buttonStyle="bg-white text-black rounded-full btn-circle btn block md:hidden" label="NL"></Button>
+          <Button buttonStyle="bg-white text-black rounded-full btn-circle btn block md:hidden" label="NL" onClick={() => setIsOpen(!IsOpen)}></Button>
         </div>
+        <div className={`flex flex-col bg-white fixed right-12 p-8 shadow-lg rounded-lg opacity-0 top-24 transition-all ease-in duration-200 ${IsOpen ? 'opacity-100' : 'right-[-200px]'}`}>
+              <ul className="flex flex-col gap-4 text-[black]">
+                  <li>Profile</li>
+                  <li>Your Nebu</li>
+                  <li>Your Tour</li>
+                  <li>Setting</li>
+              </ul>
+          </div>
         {/* <button className="p-2 btn bg-white">
         <img src="/images/icnSearch.svg" alt="search icon" />
       </button> */}
