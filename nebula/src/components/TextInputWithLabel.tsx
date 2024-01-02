cebec0edf16cdaec39cb995e7315d81c8eacb31f
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2024-01-03 00:56:44 +0700

    Add onChange attribute

--- nebula/src/components/TextInputWithLabel.tsx
+++ nebula/src/components/TextInputWithLabel.tsx
@@ -1,20 +1,32 @@
 import React from "react"
 
 interface ITextInputWithLabel {
-    textLabel?: string
-    inputType?: string //can be text, email, password
-    placeholder?: string
+  textLabel?: string
+  inputType?: string // Can be text, email, password, etc.
+  placeholder?: string
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void // Add onChange event handler
 }
 
-const TextInputWithLabel: React.FunctionComponent<ITextInputWithLabel> = ({ textLabel, inputType, placeholder }) => {
-    return(
-        <div className="form-control">
-            <label className="label">
-                <span className="label-text font-bold text-black">{textLabel}</span>
-            </label>
-            <input type={inputType} placeholder={placeholder} className="input input-bordered bg-light-grey border-black" required />
-        </div>
-    )
+const TextInputWithLabel: React.FunctionComponent<ITextInputWithLabel> = ({
+  textLabel,
+  inputType,
+  placeholder,
+  onChange,
+}) => {
+  return (
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text font-bold text-black">{textLabel}</span>
+      </label>
+      <input
+        type={inputType}
+        placeholder={placeholder}
+        className="input input-bordered bg-light-grey border-black"
+        onChange={onChange} // Apply the onChange event handler
+        required
+      />
+    </div>
+  )
 }
 
 export default TextInputWithLabel
