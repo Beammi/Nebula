136754ff3994618a7c410d7dc37967007e1bbbb1
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2024-02-17 00:22:05 +0700

    Add UpdateMapView Component

---
+++ nebula/src/components/UpdateMapView.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useMap } from 'react-leaflet';
+
+const UpdateMapView = ({ position ,zoomLevel = 15 }) => {
+    const map = useMap();
+
+    useEffect(() => {
+      if (position && map) {
+        map.flyTo(position, zoomLevel);
+        console.log("Current Position:", position); // Use `position` instead of `currentPosition`
+      }
+    }, [position, map]); // Effect runs when `position` or `map` changes
+
+    return null; // This component does not render anything itself
+};
+export default UpdateMapView;
