06f18a60b0c2e726409aac23891210c661c46c24
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2024-02-19 03:08:28 +0700

    Add LocationProvider

--- nebula/src/pages/_app.js
+++ nebula/src/pages/_app.js
@@ -1,28 +1,18 @@
-import "@/styles/globals.css"
-import "leaflet/dist/leaflet.css"
-import { ClerkProvider } from "@clerk/nextjs"
-import { SessionProvider } from "next-auth/react"
+import "@/styles/globals.css";
+import "leaflet/dist/leaflet.css";
+import { LocationProvider } from "@/contexts/LocationContext"; // Assuming LocationProvider is exported from LocationContext
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}) {
+export default function App({ Component, pageProps }) {
   return (
     <>
-      {/* <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
-        rel="stylesheet"
-      /> */}
-      {/* <SessionProvider session={session}> */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet" />
 
+      {/* Wrap your Component with LocationProvider */}
+      <LocationProvider>
         <Component {...pageProps} />
-      {/* </SessionProvider> */}
+      </LocationProvider>
     </>
-  )
+  );
 }
