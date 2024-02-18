a43f0366761ff6abe7bca349819647e774177a38
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2024-02-19 03:08:12 +0700

    Add LocationContext

---
+++ nebula/src/contexts/LocationContext.js
@@ -0,0 +1,79 @@
+// LocationContext.js
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import { getCurrentLocation, getPlaceName } from '@/utils/navigationUtils';
+
+const defaultState = {
+  currentPosition: [
+    13.7563, 100.5018
+  ],
+  setCurrentPosition: (position) => {currentPosition=position},
+  currentPlace: '',
+  setCurrentPlace: (place) => {currentPlace=place},
+};
+
+const LocationContext = createContext(defaultState);
+
+export const useLocation = () => useContext(LocationContext);
+
+export const LocationProvider = ({ children }) => {
+  const [currentPosition, setCurrentPosition] = useState([
+    13.7563, 100.5018
+  ])
+  const [currentPlace, setCurrentPlace] = useState('');
+
+  useEffect(() => {
+    async function fetchCurrentLocation() {
+      try {
+        const position = await getCurrentLocation();
+        setCurrentPosition([position[0], position[1]]);
+        // Fetch place name for the initial position
+        const placeName = await getPlaceName(position[0], position[1]);
+        setCurrentPlace(placeName);
+      } catch (error) {
+        console.error("Error getting location:", error);
+      }
+    }
+
+    function continuouslyUpdateLocation() {
+      const options = {
+        enableHighAccuracy: true,
+        maximumAge: 0,
+        timeout: 10000,
+      };
+
+      const update = async () => {
+        navigator.geolocation.getCurrentPosition(
+          async (position) => {
+            const { latitude, longitude } = position.coords;
+            setCurrentPosition([latitude, longitude]);
+            try {
+              const placeName = await getPlaceName(latitude, longitude);
+              setCurrentPlace(placeName);
+            } catch (error) {
+              console.error("Failed to fetch place name:", error);
+            }
+            setTimeout(update, 60000); // Update every 60 seconds
+          },
+          (error) => {
+            console.error(error);
+            setTimeout(update, 120000); // Retry after 2 minutes on error
+          },
+          options
+        );
+      };
+
+      update();
+    }
+
+    fetchCurrentLocation();
+    continuouslyUpdateLocation();
+  }, []);
+
+  return (
+    <LocationContext.Provider value={{ currentPosition, setCurrentPosition, currentPlace, setCurrentPlace }}>
+      {children}
+    </LocationContext.Provider>
+  );
+};
+
+export default LocationContext;
