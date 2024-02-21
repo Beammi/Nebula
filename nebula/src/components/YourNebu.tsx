b52c728cea12ae051bccdab720562bfdbafc28a8
Natthapong Lueangphumphitthaya <natthapong2100@gmail.com>
2024-02-21 14:52:03 +0700

    checkbox for select delete nebu (ver.1)

--- nebula/src/components/YourNebu.tsx
+++ nebula/src/components/YourNebu.tsx
@@ -22,6 +22,7 @@ export default function YourNebu(props) {
   const action = props.action;
   const accountName = props.accountName;
   const [showInfo, setShowInfo] = useState([false])
+  const [showSelectBox, setShowSelectBox] = useState(false)
 
   const data = [{
     "title": "Big Ben",
@@ -37,6 +38,10 @@ export default function YourNebu(props) {
   }]
 
   console.log("This is account name: ", accountName);
+
+  function handleSelect(){
+
+  }
   
 
   return (
@@ -62,22 +67,23 @@ export default function YourNebu(props) {
             <h2 className="text-2xl text-center">Your Nebu</h2>
             {data.map((data, index) => (
                 <>
-                    <div className="text-white font-normal pl-5 py-1 mt-3 bg-blue rounded-lg w-full drop-shadow-md cursor-pointer"
+                    <div className="flex flex-row text-white font-normal pl-5 py-1 mt-3 bg-blue rounded-lg w-full drop-shadow-md cursor-pointer"
                         onClick={() => {
                             const updatedShowInfo = [...showInfo]; // Make a copy of the showInfo array
                             updatedShowInfo[index] = !updatedShowInfo[index]; // Toggle the value at the specific index
                             setShowInfo(updatedShowInfo); // Update the state with the modified array
                         }}>
                             {data.title}
+                            <input type="checkbox" className="checkbox ml-auto mr-5 checkbox-accent border-2 border-white" />
                     </div>
                     <div className={`transition-all ease-in duration-00 ${showInfo[index] ? 'h-auto opacity-100' : 'h-0 opacity-0'}`}>
                         { showInfo[index] && (
                             <div className="flex flex-col bg-white mt-3 pl-5 py-2 pr-2 rounded-lg gap-y-3 drop-shadow-md">
                                 <h3 className="text-md mt-2">{data.title}</h3>
-                                <div className='flex flex-row mt-1 gap-x-2'>
-                                    <Link href="https://www.google.com/" className=" px-2 py-1 w-fit bg-yellow text-white rounded-lg normal-case border-0 text-sm cursor-pointer">#tourist_attraction</Link>
-                                    <Link href="https://www.google.com/" className=" px-2 py-1 w-fit bg-grey text-black rounded-lg normal-case border-0 text-sm cursor-pointer">#bridge</Link>
-                                    <Link href="https://www.google.com/" className=" px-2 py-1 w-fit bg-grey text-black rounded-lg normal-case border-0 text-sm cursor-pointer">#natLikes</Link>
+                                <div className='flex flex-row lg:flex-row mt-1 gap-2 flex-wrap lg:w-auto'>
+                                    <Link href="https://www.google.com/" className="px-1 lg:px-2 py-1 w-fit bg-yellow text-white rounded-lg normal-case border-0 text-sm cursor-pointer">#tourist_attraction</Link>
+                                    <Link href="https://www.google.com/" className=" px-1 lg:px-2 py-1 w-fit bg-grey text-black rounded-lg normal-case border-0 text-sm cursor-pointer">#bridge</Link>
+                                    <Link href="https://www.google.com/" className=" px-1 lg:px-2 py-1 w-fit bg-grey text-black rounded-lg normal-case border-0 text-sm cursor-pointer">#natLikes</Link>
                                 </div>
                                 <p className="font-normal">{data.description}</p>
                                 <div className="flex gap-x-2 w-2/3 overflow-x-scroll">
@@ -90,7 +96,7 @@ export default function YourNebu(props) {
                     </div>
                 </>
             ))}
-            <button className="rounded-lg mt-16 py-2 px-4 normal-case font-normal text-white ml-auto mr-5 bg-blue">Select</button>
+            <button className="rounded-lg mt-16 py-2 px-4 normal-case font-normal text-white ml-auto mr-5 bg-blue" onClick={() => setShowSelectBox(true)}>Select</button>
             
             
         </div>
