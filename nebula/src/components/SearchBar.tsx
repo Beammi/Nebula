e94f3d26dab40535c1d724d5f9a6fd8c34dccf68
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2024-04-01 00:46:24 +0700

    Use old SearchBar

--- nebula/src/components/SearchBar.tsx
+++ nebula/src/components/SearchBar.tsx
@@ -6,26 +6,18 @@ import smallPin from "../../public/images/smallPin.png"
 import smallFlag from "../../public/images/smallFlag.png"
 import smallUser from "../../public/images/smallUser.png"
 import smallTag from "../../public/images/smallTag.png"
-import smallThinPin from "../../public/images/placePinIcon.png"
 import TagSuggestion  from "@/components/TagSuggestion"
 import AccountProfile from "@/components/AccountProfile"
-import ViewTourList from "@/components/ViewTourList"
 import Bookmark from "@/components/Bookmark"
 import MyNebu from "@/components/MyNebu"
 import MyTour from "@/components/MyTour"
 import Profile from "@/components/Profile"
-import PlaceInfoPanel from "@/components/nebu/PlaceInfoPanel"
 import { useRouter } from "next/router"
-import { useLocation } from "@/contexts/LocationContext"
 import React from "react"
-
-
 interface ISearchBar {
   text?: string
 }
-
 const SearchBar: React.FunctionComponent<ISearchBar> = ({ text }) => {
-
   const router = useRouter()
   const [IsOpen, setIsOpen] = useState(false)
   const [inputValue, setInputValue] = useState("");
@@ -36,190 +28,141 @@ const SearchBar: React.FunctionComponent<ISearchBar> = ({ text }) => {
   const [showMyTour, setShowMyTour] = useState(false);
   const [showBookmark, setShowBookmark] = useState(false);
   const [showMyProfile, setShowMyProfile] = useState(false);
-  const [showViewTourList, setShowViewTourList] = useState(false)
-  const [showPlaceInfoPanel, setShowPlaceInfoPanel] = useState(false)
   const [tagSuggestValue, setTagSuggestValue] = useState("");
   const [accountNameValue, setAccountNameValue] = useState("");
-  const [nebu, setNebu] = useState([]);
-  const [api, setApi] = useState<{ value: string; type: string }[]>([]);
   const [suggestions, setSuggestions] = useState<{ value: string; type: string }[]>([]);
-  const {
-    currentPlace,
-    setCurrentPlace,
-    setEnableContinuousUpdate,
-    setCurrentPosition,
-  } = useLocation()
-
   function closeTagSuggestion() {
     setShowTagSuggestion(false);
   }
-
   function closeAccountProfile() {
     setShowAccountProfile(false);
   }
-
   function closeMyNebu() {
     setShowMyNebu(false);
   }
-
   function handleMyNebuClick(){
     setAccountNameValue("nat2100")
     setShowMyNebu(true)
   }
-
   function closeMyTour() {
     setShowMyTour(false);
   }
-
   function handleMyTourClick(){
     setAccountNameValue("nat2100")
     setShowMyTour(true)
   }
-
   function closeBookmark() {
     setShowBookmark(false);
   }
-
   function handleBookmarkClick(){
     setAccountNameValue("nat2100")
     setShowBookmark(true)
   }
-
   function closeMyProfile() {
     setShowMyProfile(false);
   }
-
   function handleMyProfileClick(){
     setAccountNameValue("nat2100")
     setShowMyProfile(true)
   }
-
-  function closeViewTourList() {
-    setShowViewTourList(false);
-  }
-
-  function closePlaceInfoPanel() {
-    setShowPlaceInfoPanel(false);
-  }
-
   // Function to handle suggestion click
-  // const handleSuggestionClick = (suggestion: { value: string; type: string }) => {
-  const handleSuggestionClick = async (suggestion) => {
-    
+  const handleSuggestionClick = (suggestion: { value: string; type: string }) => {
     if(suggestion.type === "tag") {
-      setInputValue(suggestion.value);
       setShowTagSuggestion(true);
-
-      // setShowViewTourList(true)
-
-      setTagSuggestValue(suggestion.value)      
-
+      setTagSuggestValue(suggestion.value)
     }
     else if(suggestion.type === "user"){
-      setInputValue(suggestion.value);
       // const fullPath = `/${suggestion.value}`
       // router.push(fullPath)
       setShowAccountProfile(true)
       setAccountNameValue(suggestion.value)
-
     }
-    else if(suggestion.type === "place"){
-      setInputValue(suggestion.value.display_name);
-      setCurrentPosition([parseFloat(suggestion.value.lat), parseFloat(suggestion.value.lon)])
-      setCurrentPlace(suggestion.value.display_name)
+    else{
+      setInputValue(suggestion.value);
     }
-    else if(suggestion.type === "nebu"){
-      setInputValue(suggestion.value.title);
-      setCurrentPosition([parseFloat(suggestion.value.latitude), parseFloat(suggestion.value.longitude)])
-      
-      setShowPlaceInfoPanel(true)
-      // setPlaceInfoName(suggestion.value.title)
-      setNebu(suggestion.value)    
-    }
-
     setShowSuggestions(false);
   };
+  // Function to handle input change
+  const handleInputChange = async(event: React.ChangeEvent<HTMLInputElement>) => {
+    const formattedData: { value: string, type: string }[] = [];
+    const key = event.target.value;
+    setInputValue(key)
+    // console.log("K: ", key, ", ", key);
 
-  // Function to fetch data from API
-  const fetchData = async (key) => {
-    try {
-      // Fetch data from multiple endpoints
-      const [nebuResponse, tagResponse, userResponse, nominatimResponse] = await Promise.all([
-        fetch(`/api/search/getNebuByKeyword?searchKey=${key}`),
-        fetch(`/api/search/getTagByKeyword?searchKey=${key}`),
-        fetch(`/api/search/getUsersByDisplayName?searchKey=${key}`),
-        fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(key.replace(/\s/g, '+'))}`, {
-          method: 'GET',
-          headers: {
-            'User-Agent': 'Nebula/1.0 (63011290@kmitl.ac.th)',
-          },
-        }),
-      ]);
 
-      // Parse responses
-      const [nebuData, tagData, userData, nominatimData] = await Promise.all([
-        nebuResponse.json(),
-        tagResponse.json(),
-        userResponse.json(),
-        nominatimResponse.json(),
-      ]);
-
-      console.log('Nebu data:', nebuData);
-      console.log('Tag data:', tagData);
-      console.log('User data:', userData);
-      // console.log('Nominatim data:', nominatimData);
-
-      // Process data and update API state
-      // const formattedData = [];
-      const formattedData: { value: string, type: string }[] = [];
-      if (nebuData.length > 0) {
-        nebuData.map((d) => formattedData.push({ value: d, type: 'nebu' }));
-      }
-      if (tagData.length > 0) {
-        tagData.map((name) => formattedData.push({ value: name, type: 'tag' }));
-      }
-      if (userData.length > 0) {
-        userData.map((d) => formattedData.push({ value: d.display_name, type: 'user' }));
-      }
-      if (nominatimData.length > 0) {
-        nominatimData.map((d) => formattedData.push({ value: d, type: 'place' }));
-      }
-      
-      setApi(formattedData);
-
-    } catch (error) {
-      console.error('Fetch error:', error);
-    }
-  };
-
-  const handleInputChange = (event) => {
-    const key = event.target.value
-    setInputValue(key);
-
-    if (key.trim().length === 0 || key == null) {
-      setSuggestions([]); // Clear suggestions if input is empty
+    if (key.trim().length === 0 || key == null) { // Clear suggestions if input is empty
       setShowSuggestions(false);
-      return;
+      setSuggestions([]);
+      return; // Exit early to prevent further execution of the function
     }
 
-    setShowSuggestions(true)
+    // if(key === ""){
+    //   setShowSuggestions(false)
+    // }
+    // if (key === "") { // Clear suggestions if input is empty
+    //   // setSuggestions([]);
+    //   setShowSuggestions(false);
+    //   setTimeout(() => setSuggestions([]), 100); // Introduce a slight delay before clearing suggestions
+    //   return;
+    // }
+    if(key !== ""){    
+    
+      let url = `/api/nebu/getUsersByDisplayName?searchKey=${key}`
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        const data: string[] = await response.json(); // normal array same as write in api
+        data.forEach(name => formattedData.push({ value: name, type: "user" }));
+      } catch (error) {
+        console.error("Fetch error:", error)
+      }
+      url = `/api/nebu/getTagByKeyword?searchKey=${key}`
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        const data: string[] = await response.json();             
+        // const formattedData = data.map(name => ({ value: name, type: "tag" }));
+        data.forEach(name => formattedData.push({ value: name, type: "tag" }));
+      } catch (error) {
+        console.error("Fetch error:", error)
+      }
+
+      url = `/api/nebu/getNebuByKeyword?searchKey=${key}`
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        const data: string[] = await response.json();             
+        // const formattedData = data.map(name => ({ value: name, type: "tag" }));
+        data.forEach(name => formattedData.push({ value: name, type: "nebu" }));
+
+      } catch (error) {
+        console.error("Fetch error:", error)
+      }
+
+      setSuggestions(formattedData)        
+      setShowSuggestions(suggestions.length !== 0);
+      console.log(suggestions);  
+    }
+
+
   };
-
   useEffect(() => {
-    console.log("API:", api);
-    setSuggestions(api)
-  }, [api]);
-
-  useEffect(() => {
-    fetchData(inputValue)
+    // Check if input is empty after setting suggestions
+    if (inputValue === "") {
+      setShowSuggestions(false);
+      setSuggestions([]);
+    }
   }, [inputValue]);
   
-  
-
   return (
     
-      <div className="relative w-full 2xl:w-[25%] lg:w-2/5 md:w-1/4 sm:w-3/4 p-4">
-      {/* // <div className="relative w-full 2xl:w-[30%] lg:w-2/5 md:w-1/4 sm:w-3/4 p-4 max-w-lg"> */}
+      <div className="relative w-full 2xl:w-[30%] lg:w-2/5 md:w-1/4 sm:w-3/4 p-4 max-w-md">
         <input
           type="text"
           className="pl-10 pr-4 py-2 w-full input input-bordered bg-[#fefefe] text-black shadow-neutral-500 shadow-sm"
@@ -235,17 +178,17 @@ const SearchBar: React.FunctionComponent<ISearchBar> = ({ text }) => {
         <div className="absolute right-8 top-8 transform -translate-y-1/2">
           <Button buttonStyle="bg-white text-black rounded-full btn-circle btn block md:hidden" label="NL" type="button" onClick={() => setIsOpen(!IsOpen)}></Button>
         </div>
-
-        {showSuggestions && (          
-          <div className="absolute mt-2 w-full max-h-[500px] overflow-y-auto bg-white shadow-lg rounded-lg text-black transition-opacity delay-300 duration-500 ease-in-out opacity-100">
+        {showSuggestions && (
+          // <div className="absolute mt-2 w-full bg-white shadow-lg rounded-lg text-black">
+          <div className="absolute mt-2 w-full bg-white shadow-lg rounded-lg text-black transition-opacity delay-300 duration-500 ease-in-out opacity-100">
             {suggestions.map((suggestion, index) => (
               <div className="flex flex-row" key={index}>
                 <div
-                  className="py-2 pl-4 cursor-pointer hover:bg-gray-400 flex items-center gap-x-3 flex-shrink-0"
+                  className="py-2 pl-4 cursor-pointer hover:bg-gray-400 flex items-center gap-x-3"
                   onClick={() => handleSuggestionClick(suggestion)}
                 >
                   {(suggestion.type === "nebu") && 
-                    <figure><Image src={smallPin} alt="pic" className="" width={23}/></figure>
+                    <figure><Image src={smallPin} alt="pic" className="" width={20}/></figure>
                   }
                   {(suggestion.type === "tour") && 
                     <figure><Image src={smallFlag} alt="pic" className="" width={15}/></figure>
@@ -256,32 +199,12 @@ const SearchBar: React.FunctionComponent<ISearchBar> = ({ text }) => {
                   {(suggestion.type === "user") && 
                     <figure><Image src={smallUser} alt="pic" className="" width={18}/></figure>
                   }
-                  {(suggestion.type === "place") && 
-                    <figure><Image src={smallThinPin} alt="pic" className="-ml-0.5" width={24}/></figure>
-                  }
-                  {(suggestion.type === "place") && 
-                    <div>{suggestion.value.display_name}</div>
-                  }
-                  {(suggestion.type === "nebu") && 
-                    <div>{suggestion.value.title}</div>
-                  }
-                  {(suggestion.type === "tour") && 
-                    <div>{suggestion.value}</div>
-                  }
-                  {(suggestion.type === "tag") && 
-                    <div>{suggestion.value}</div>
-                  }
-                  {(suggestion.type === "user") && 
-                    <div>{suggestion.value}</div>
-                  }                  
+                  <span>{suggestion.value}</span>
                 </div>
               </div>
             ))}
           </div>
         )}
-
-        <PlaceInfoPanel toggle={showPlaceInfoPanel} action={closePlaceInfoPanel} nebu={nebu}/>              
-        <ViewTourList toggle={showViewTourList} action={closeViewTourList} name={tagSuggestValue}/>
         <TagSuggestion toggle={showTagSuggestion} action={closeTagSuggestion} tagName={tagSuggestValue}/>
         <AccountProfile toggle={showAccountProfile} action={closeAccountProfile} accountName={accountNameValue}/>
         
@@ -289,7 +212,6 @@ const SearchBar: React.FunctionComponent<ISearchBar> = ({ text }) => {
         <MyTour toggle={showMyTour} action={closeMyTour} accountName={accountNameValue}/>
         <Bookmark toggle={showBookmark} action={closeBookmark} accountName={accountNameValue}/>
         <Profile toggle={showMyProfile} action={closeMyProfile} accountName={accountNameValue}/>
-
         <div className={`flex flex-col bg-white fixed right-12 p-8 shadow-lg rounded-lg opacity-0 top-24 transition-all ease-in duration-200 ${IsOpen ? 'opacity-100' : 'right-[-200px]'}`}>
               <ul className="flex flex-col gap-4 text-[black]">
                 <li className="cursor-pointer" onClick={() => handleMyProfileClick()}>Profile</li>
